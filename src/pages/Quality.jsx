63d26d31886658cd36ec1b102a3e831b2c94e111
Rushi <rushikesh@omsinv.com>
2025-06-12 00:23:11 +0530

    contact us page

--- src/pages/Quality.jsx
+++
@@ -1,9 +0,0 @@
-import SectionWrapper from "../components/SectionWrapper";
-
-const Quality = () => (
-  <SectionWrapper title="Quality">
-    Quality is non-negotiable for us...
-  </SectionWrapper>
-);
-
-export default Quality;
